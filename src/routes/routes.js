86aaa0d9a797eb1f4c82ea8ccd2e5ead0e18faab
Deivid R <deivid.rosario@hotmail.com>
2021-06-12 15:09:46 -0300

    config route auth

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,7 +8,7 @@ routes.get('/userss',auth,controller.find);
 routes.put('/api/users/:id', auth,controller.update);
 routes.delete('/api/users/:id',controller.delete);
 routes.get('/users/:email',controller.show);
-//routes.post('/auth',controller.findOne)
+routes.post('/auth',controller.findOne)
 routes.get('/user/:email',controller.findOne);
 
 module.exports = routes;
