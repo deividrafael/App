0c9b6867256ae7af96733f09937bd0af4f340d62
Deivid R <deivid.rosario@hotmail.com>
2021-06-14 02:35:01 -0300

    Config routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,8 +4,8 @@ const controller = require('../controller/controller')
 const auth = require('../middleware/auth')
 //API
 routes.post('/api/users',controller.create);
-routes.get('/userss',auth,controller.find);
-routes.put('/api/users/:id', auth,controller.update);
+routes.get('/users',controller.find);
+routes.put('/api/users/:id', controller.update);
 routes.delete('/api/users/:id',controller.delete);
 routes.get('/users/:email',controller.show);
 routes.post('/auth',controller.findOne)
